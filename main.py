64c103077ae2c9f2cc6ba113c781c04fa822da5f
Elvin Ibrahimov <44234846+elvinibrahimov@users.noreply.github.com>
2018-10-19 14:40:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 #тест
+l = [i  for i in range(10)]
+print(l)
+print(type(l))
+
+d={i: i**6 for i in range(10)}
+print(d)
+print(type(d))
+
+s={i for i in range(10)}
+print(s)
+print(type(s))
+
+g=(i for i in range(10))
+print(g)
+#print(type
+
+points = []
+for x in range(5):
+	for y in range(3):
+		points.append((x, y))
+
+print(points)
+
+
+def hypervalue(*args):
+	print(args)
+	print(type(args))
+
+hypervalue(23,4,5,5,34,'sd',"hello")
